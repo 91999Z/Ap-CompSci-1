24c6b5573510ecf512bfb34d173caa047a70ed0c
Evan Dockter <evan.dockter@outlook.com>
2026-09-17 09:45:53 -0400

    Add files via upload

---
+++ APCS Course Files/Module 01/Mod01 Lessons/01.02 HelloWorld/helloWorld.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class helloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class helloWorld
+{ 
+    public static void main (String[] args){
+        
+        System.out.println("Hello, World!");
+        System.out.println("Its a great day for programming.");
+
+    /**System.out.print("Hello, World!");
+    System.out.println();
+    System.out.println("Its a great day for programming.");
+    */
+    
+    }
+}
